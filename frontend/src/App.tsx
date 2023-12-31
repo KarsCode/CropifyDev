bdde9caf138ca1915be8cd3c73edbaee018009c2
KarsCode <kartiksubbaram18@gmail.com>
2023-12-31 16:34:52 +0530

    BRUH

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,6 @@ import PlantDisease from './pages/PlantDisease';
 import Layout from './Layout';
 import Home from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
-import PlantDiseases from './pages/PlantDiseases';
-import SoilType from './pages/SoilType';
-import SoilTypes from './pages/SoilTypes';
 
 function App() {
 
@@ -22,7 +19,7 @@ function App() {
         <Route path="/Contact" element={<ContactPage/>} />
         <Route path="/Profile" element={<div>Profile</div>} />
         <Route path="/plantDisease/:id" element={<PlantDisease/>} />
-        <Route path="/soilType/:id" element={<SoilType/>} />
+        
       </Route>
     </Routes>
     
