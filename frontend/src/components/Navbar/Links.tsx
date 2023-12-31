3bab7298da4fa0115b4f655587e1c8b4498c5891
AnirveshArcot <arcotanirvesh@gmail.com>
2023-12-31 21:05:33 +0530

    Data Fetched from DB

--- frontend/src/components/Navbar/Links.tsx
+++ frontend/src/components/Navbar/Links.tsx
@@ -16,7 +16,7 @@ const links = [
   },
   {
     title: "Blog",
-    path: "/BlogPage",
+    path: "/Blog",
   },
   {
     title: "Contact",
--- frontend/src/pages/PlantDisease.tsx
+++ frontend/src/pages/PlantDisease.tsx
@@ -1,14 +1,44 @@
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { VITE_API_URL } from "../setupEnv";
+import { useEffect, useState } from "react";
 
-export default function IndexPage(){
-    const {id} = useParams();
+export default function PlantDisease() {
+  const { id } = useParams();
+  const [data, setData] = useState(null);
 
-    // make request to api get json of data
-    return(
-        <>
-        {id}
-        </>
-    )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${VITE_API_URL}/plantDisease/${id}`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Only fetch data if id is present
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
+  // Render loading state if data is still fetching
+  if (!data || !data["document"]) {
+    return <p>Loading...</p>;
+  }
+
+  console.log(data);
+if(data["document"]){
+  return (
+    <>
+      {id}
+      {/* Render your component using the fetched data */}
+    </>
+  );
 }
-
-
+  
+}
