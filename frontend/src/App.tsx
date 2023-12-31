fb3d33fae84b1e83c83c7e05dad2b4c4f43d9825
KarsCode <kartiksubbaram18@gmail.com>
2023-12-31 16:24:55 +0530

    minor routing change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/CropDoctor" element={<div>CropDoctor</div>} />
         <Route path="/SoilCheck" element={<div>SoilCheck</div>} />
-        <Route path="/Blog" element={<div>Blog</div>} />
+        <Route path="/BlogPage" element={<div>Blog</div>} />
         <Route path="/Contact" element={<ContactPage/>} />
         <Route path="/Profile" element={<div>Profile</div>} />
         <Route path="/plantDisease/:id" element={<PlantDisease/>} />
