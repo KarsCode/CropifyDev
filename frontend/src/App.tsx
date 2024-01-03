e2da1c03efe5cdf74db0541e11349bdb1587e4e1
KarsCode <kartiksubbaram18@gmail.com>
2024-01-03 06:50:02 +0530

    Blog Page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import ContactPage from './pages/ContactPage';
 import SoilType from './pages/SoilType';
 import SoilCheck from './pages/SoilCheck';
 import CropDoctor from './pages/CropDoctor';
+import BlogPage from './pages/BlogPage';
+import SingleBlogPost from './pages/SingleBlogPost';
 
 function App() {
 
@@ -17,7 +19,8 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/CropDoctor" element={<CropDoctor/>} />
         <Route path="/SoilCheck" element={<SoilCheck/>} />
-        <Route path="/Blog" element={<div>Blog</div>} />
+        <Route path="/Blog" element={<BlogPage/>} />
+        <Route path="/Blog/pageId" element={<SingleBlogPost/>} />
         <Route path="/Contact" element={<ContactPage/>} />
         <Route path="/Profile" element={<div>Profile</div>} />
         <Route path="/plantDisease/:id" element={<PlantDisease/>} />
