a010344f26744f86def38d602c0f5c5f21d23f60
Gaurav Pradeep <gauravpradeep2004@gmail.com>
2024-01-06 12:33:17 +0530

    fire detection python

---
+++ fire_detection.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+    resized_image = cv2.resize(image, (256, 256))
+    resized_image = resized_image/255
+    input_image = np.expand_dims(resized_image, axis=0)   
+    return input_image
+
+def get_predictions(model, input_image):
+    predictions = model.predict(input_image)
+    return predictions
+
+model_path = 'pretrained_weights/fire_detection.h5'
+model = load_model(model_path)
+model.compile(optimizer='adam', loss='binary_crossentropy', 
+metrics=['accuracy'])
+image_path = 'images/test_fire.jpg'
+input_image = preprocess_image(image_path)
+predictions = get_predictions(model, input_image)
+
+if(predictions>=0.5):
+	print(1)
+else:
+	print(0)
