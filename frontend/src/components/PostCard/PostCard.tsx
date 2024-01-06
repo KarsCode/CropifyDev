eab9e5ba5a787c9845cfdf9048a1778834796b13
Naman <naman.kmr246@gmail.com>
2024-01-06 11:09:57 +0530

    loading

--- frontend/src/components/PostCard/PostCard.tsx
+++ frontend/src/components/PostCard/PostCard.tsx
@@ -2,28 +2,31 @@ import React from 'react';
 import { Button } from '../ui/button';
 import { Link } from 'react-router-dom';
 
-
 interface DocumentInterface {
   title: string;
   description: string;
   article: string;
   date: string;
   image: string;
-  blog_id:number;
+  blog_id: number;
 }
 
 interface PostCardProps {
-
   document: DocumentInterface;
 }
 
-const PostCard: React.FC<PostCardProps> = ( {document} ) => {
-  const link = "/Blog/"+document.blog_id.toString();
+const PostCard: React.FC<PostCardProps> = ({ document }) => {
+  const link = '/Blog/' + document.blog_id.toString();
+
   return (
     <div className='flex flex-col gap-10 mb-5'>
       <div className='flex relative'>
-        <div className='w-[90%] relative'>
-          <img src={document.image} style={{ objectFit: 'cover' }} alt='Blog Post' />
+        <div className='w-[90%] relative shadow-md' style={{ width: '400px', height: '280px' }}>
+          <img
+            src={document.image}
+            style={{ objectFit: 'cover', width: '100%', height: '100%', borderRadius: '10px'}}
+            alt='Blog Post'
+          />
         </div>
         {/* Date */}
         <div className='font-extralight rotate-[90deg] m-auto'>{document.date}</div>
@@ -35,14 +38,14 @@ const PostCard: React.FC<PostCardProps> = ( {document} ) => {
 
         {/* Desc */}
         <p className='w-[90%] mb-5 font-light'>{document.description}</p>
+        </div>
 
         <div className='pt-4 pb-4'>
-          <Link to={link}>
+          <Link to={link} className='shadow-md'>
             <Button>READ MORE</Button>
           </Link>
         </div>
-      </div>
-    </div>
+        </div>
   );
 };
 
